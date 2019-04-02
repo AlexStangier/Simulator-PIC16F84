10515aa6a44173bf6d7f452753b0bd227ddf5525
AlexStan <Alex.stangier@gmail.com>
2019-04-02 15:15:25 +0200

    changes to visibility first implementation of registers

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,4 +1,5 @@
 package controller;
+
 import view.*;
 import model.*;
 
--- src/model/Register.java
+++ src/model/Register.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Register {
+import controller.Main;
+
+public class Register extends Main {
+
+    //Initialization of RAM storage
+    byte[] ram_Bank0 = new byte[128];
+    byte[] ram_Bank1 = new byte[128];
+
+    //Initialization of the Programm Counter
+    byte programm_Counter = 0;
+
+    //Initialization of Programm Memory
+    short[] programm_Memory = new short[14336];  //1024 x 14B
+
+    //Initialization of the Working Register
+    byte working_Register = 0;
+
+
+
+
 }
