b8516eb48310de68edfe082b78fd304fd57df6f3
AlexStan <alex.stangier@gmail.com>
2019-04-15 16:33:41 +0200

    Working implementation of the Decoder class

--- src/controller/Decoder.java
+++ src/controller/Decoder.java
@@ -3,134 +3,277 @@ package controller;
 
 public class Decoder {
 
-    static Operation determineMoreTypes(int operationCode) {
-
-        Operation operationBlock = new Operation();
-        int moreTypesOfOperations = 0b11_1100_0000_0000;
-        int toCheck = operationCode & moreTypesOfOperations;
-
+    void determineOperationType(int opCode, Operation op) {
+        int toCheck = opCode & 0b11_0000_0000_0000; //11_0000_0000_0000
 
         switch (toCheck) {
-            case 0x0009:
-                operationBlock.type = OperationType.RETFIE;
+            case 0x0:
+                op.setTypeDecider(OperationTypeDecider.BYTE);
                 break;
-            case 0x0063:
-                operationBlock.type = OperationType.SLEEP;
-                break;
-            case 0x0064:
-                operationBlock.type = OperationType.CLRWDT;
-                break;
-            case 0x0080:
-                operationBlock.type = OperationType.MOVWF;
-                break;
-        }
-        return operationBlock;
-    }
-
-
-    static Operation determineType(int operationCode) {
-        Operation operationBlock = new Operation();
-
-        int typeOfOperation = 0b11_0000_0000_0000;
-        int moreTypesOfOperations = 0b11_1100_0000_0000;
-
-        //Byte Oriented File Register Operation
-        int byteOrientedMask = 0b11_1111_0000_0000;
-        int destinationBit = 0b00_0000_0100_000000;
-        int byteOrientedAddress = 0b0000000011111111;
-
-        //Bit Oriented File Register Operation
-        int bitOrientedMask = 0b10_1100_0000_0000;
-
-        int toCheck = operationCode & typeOfOperation;
-        int toCheckMore = operationCode & moreTypesOfOperations;
-
-        switch (toCheck) {
-            case 0x0000:
-                operationBlock.type = OperationType.NOP;
-                break;
-            case 0x0100:
-                operationBlock.type = OperationType.CLRF;
-                break;
-            case 0x0200:
-                operationBlock.type = OperationType.SUBWF;
-                break;
-            case 0x0300:
-                operationBlock.type = OperationType.DECF;
-                break;
-            case 0x0A00:
-                operationBlock.type = OperationType.INCF;
-                break;
-            case 0x0B00:
-                operationBlock.type = OperationType.DECFSZ;
-                break;
-            case 0x0C00:
-                operationBlock.type = OperationType.RRF;
-                break;
-            case 0x0D00:
-                operationBlock.type = OperationType.RLF;
-                break;
-            case 0x0E00:
-                operationBlock.type = OperationType.SWAPF;
-                break;
-            case 0x0F00:
-                operationBlock.type = OperationType.INCFSZ;
-                break;
-            case 0x0180:
-                operationBlock.type = OperationType.ANDWF;
-                break;
-            case 0x0400:
-                operationBlock.type = OperationType.IORWF;
-                break;
-            case 0x0500:
-                operationBlock.type = OperationType.ADDWF;
-                break;
-            case 0x0600:
-                operationBlock.type = OperationType.XORWF;
-                break;
-            case 0x0700:
-                operationBlock.type = OperationType.ADDWF;
-                break;
-            case 0x0800:
-                operationBlock.type = OperationType.MOVF;
-                break;
-            case 0x0900:
-                operationBlock.type = OperationType.COMF;
-                break;
-            case 0x2000:
-                operationBlock.type = OperationType.CALL;
-                break;
-            case 0x2100:
-                operationBlock.type = OperationType.CALL;
-                break;
-            case 0x2800:
-                operationBlock.type = OperationType.GOTO;
+            case 0x1000:
+                op.setTypeDecider(OperationTypeDecider.BIT);
                 break;
             case 0x3000:
-                operationBlock.type = OperationType.MOVLW;
-                break;
-            case 0x3400:
-                operationBlock.type = OperationType.RETLW;
-                break;
-            case 0x3800:
-                operationBlock.type = OperationType.IORLW;
-                break;
-            case 0x3900:
-                operationBlock.type = OperationType.ANDLW;
-                break;
-            case 0x3A00:
-                operationBlock.type = OperationType.XORLW;
-                break;
-            case 0x3C00:
-                operationBlock.type = OperationType.SUBLW;
-                break;
-            case 0x3E00:
-                operationBlock.type = OperationType.ADDLW;
+                op.setTypeDecider(OperationTypeDecider.LITERAL);
                 break;
             default:
-                operationBlock = determineMoreTypes(operationCode);
+                op.setTypeDecider(OperationTypeDecider.CONTROL);
                 break;
         }
-        return operationBlock;
+        if (op.typeDecider.equals(OperationTypeDecider.BYTE)) {
+            switch (opCode & 0b00_0000_1111_1111) {
+                case 0x0008:
+                    op.setTypeDecider(OperationTypeDecider.CONTROL);
+                    break;
+                case 0x0009:
+                    op.setTypeDecider(OperationTypeDecider.CONTROL);
+                    break;
+                case 0x0063:
+                    op.setTypeDecider(OperationTypeDecider.CONTROL);
+                    break;
+                case 0x0064:
+                    op.setTypeDecider(OperationTypeDecider.CONTROL);
+                    break;
+
+            }
+
+        } else if (op.typeDecider.equals(OperationTypeDecider.BYTE)) {
+            if ((opCode & 0b00_0000_1000_0000) == 0x0080) {
+                op.setTypeDecider(OperationTypeDecider.CONTROL);
+            }
+        }
+
+    }
+
+    int byteMask(int opCode) {
+        int toReturn = opCode & 0b11_1111_0000_0000;
+        return toReturn;
+    }
+
+    int bitMask(int opCode) {
+        int toReturn = opCode & 0b11_1100_0000_0000;
+        return toReturn;
+    }
+
+    int literalMask(int opCode) {
+        int toReturn = (opCode & 0b11_1111_0000_0000);
+        return toReturn;
+    }
+
+    int controlMask(int opCode) {
+        int toReturn = opCode & 0b11_1000_0000_0000;
+        return toReturn;
+    }
+
+    void handleByte(int opCode, Operation op) {
+
+        determineDestinationBit(opCode, op);
+        assignByteAddress(opCode, op);
+
+        switch (byteMask(opCode)) {
+            case 0x0700:
+                op.setType(OperationType.ADDWF);
+                break;
+            case 0x0500:
+                op.setType(OperationType.ANDWF);
+                break;
+            case 0x0100:
+                if (op.destinationBit == 1) {
+                    op.setType(OperationType.CLRF);
+                } else if (op.destinationBit == 0) {
+                    op.setType(OperationType.CLRW);
+                }
+                break;
+            case 0x0900:
+                op.setType(OperationType.COMF);
+                break;
+            case 0x300:
+                op.setType(OperationType.DECF);
+                break;
+            case 0x0B00:
+                op.setType(OperationType.DECFSZ);
+                break;
+            case 0x0A00:
+                op.setType(OperationType.INCF);
+                break;
+            case 0x0F00:
+                op.setType(OperationType.INCFSZ);
+                break;
+            case 0x0400:
+                op.setType(OperationType.IORWF);
+                break;
+            case 0x0800:
+                op.setType(OperationType.MOVF);
+                break;
+            case 0x0080:
+                op.setType(OperationType.MOVWF);
+                op.setDestinationBit(1);
+                break;
+            case 0x0000:
+                op.setType(OperationType.NOP);
+                op.setDestinationBit(0);
+                break;
+            case 0x0D00:
+                op.setType(OperationType.RLF);
+                break;
+            case 0x0C00:
+                op.setType(OperationType.RRF);
+                break;
+            case 0x0200:
+                op.setType(OperationType.SUBWF);
+                break;
+            case 0x0E00:
+                op.setType(OperationType.SWAPF);
+                break;
+            case 0x0600:
+                op.setType(OperationType.XORWF);
+                break;
+        }
+
+        if ((opCode & 0b00_0000_1000_0000) == 0x80 && op.getType().equals(OperationType.NOP)) {
+            op.setType(OperationType.MOVWF);
+            op.setDestinationBit(1);
+        }
+
+    }
+
+    void handleBit(int opCode, Operation op) {
+
+        assignBitAddress(opCode, op);
+        assignBitindex(opCode, op);
+
+        switch (bitMask(opCode)) {
+            case 0x1000:
+                op.setType(OperationType.BCF);
+                break;
+            case 0x1400:
+                op.setType(OperationType.BSF);
+                break;
+            case 0x1800:
+                op.setType(OperationType.BTFSC);
+                break;
+            case 0x1C00:
+                op.setType(OperationType.BTFSS);
+                break;
+        }
+    }
+
+    void handleLiteral(int opCode, Operation op) {
+
+        assignLiteralAddress(opCode, op);
+
+        switch (literalMask(opCode)) {
+            case 0x3E00:
+                op.setType(OperationType.ADDLW);
+                break;
+            case 0x3900:
+                op.setType(OperationType.ANDLW);
+                break;
+            case 0x3800:
+                op.setType(OperationType.IORLW);
+                break;
+            case 0x3000:
+                op.setType(OperationType.MOVLW);
+                break;
+            case 0x3400:
+                op.setType(OperationType.RETLW);
+                break;
+            case 0x3C00:
+                op.setType(OperationType.SUBLW);
+                break;
+            case 0x3A00:
+                op.setType(OperationType.XORLW);
+                break;
+        }
+    }
+
+    void handleControl(int opCode, Operation op) {
+
+        assignControlAddress(opCode, op);
+
+        switch (opCode & 0b00_0000_1111_1111) {
+            case 0x0008:
+                op.setType(OperationType.RETURN);
+                break;
+            case 0x0009:
+                op.setType(OperationType.RETFIE);
+                break;
+            case 0x0063:
+                op.setType(OperationType.SLEEP);
+                break;
+            case 0x0064:
+                op.setType(OperationType.CLRWDT);
+                break;
+        }
+
+        if ((opCode & 0b00_0000_1000_0000) == 0x80) {
+            op.setType(OperationType.MOVWF);            //extra check
+            op.setDestinationBit(1);
+        }
+
+        switch (controlMask(opCode)) {
+            case 0x2800:
+                op.setType(OperationType.GOTO);
+                break;
+            case 0x2000:
+                op.setType(OperationType.CALL);
+                break;
+        }
+    }
+
+    void determineCommand(int opCode, Operation op) {
+
+        switch (op.typeDecider) {
+            case BYTE:
+                handleByte(opCode, op);
+                break;
+            case BIT:
+                handleBit(opCode, op);
+                break;
+            case LITERAL:
+                handleLiteral(opCode, op);
+                break;
+            case CONTROL:
+                handleControl(opCode, op);
+                break;
+        }
+
+
+    }
+
+    void determineDestinationBit(int opCode, Operation op) {
+        int toCheck = opCode & 0b00_0000_1000_0000;
+        switch (toCheck) {
+            case 0x80:
+                op.setDestinationBit(1);
+                break;
+            default:
+                op.setDestinationBit(0);
+        }
+    }
+
+    void assignByteAddress(int opCode, Operation op) {
+        int toCheck = opCode & 0b00_0000_0111_1111;
+        op.setFileAddress(toCheck);
+    }
+
+    void assignBitAddress(int opCode, Operation op) {
+        int toCheck = opCode & 0b00_0000_0111_1111;
+        op.setFileAddress(toCheck);
+    }
+
+    void assignBitindex(int opCode, Operation op) {
+        int toCheck = opCode & 0b00_0011_1000_0000;
+        op.setBitIndex(toCheck);
+    }
+
+    void assignLiteralAddress(int opCode, Operation op) {
+        int toCheck = opCode & 0b00_0000_1111_1111;
+        op.setLiteral(toCheck);
+    }
+
+    void assignControlAddress(int opCode, Operation op) {
+        int toCheck = opCode & 0b00_0111_1111_1111;
+        op.setFileAddress(toCheck);
     }
 }
